eea567959153b301c137c69332cde69e7753fd56
Wang Yufan <2339497599@qq.com>
2024-04-20 19:35:05 +0800

    项目骨架搭建

---
+++ backend/src/main/java/com/kob/backend/Controller/pk/BotInfoController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.Controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    @RequestMapping("getBotInfo/")
+    public Map<String, String> getBotInfo(){
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "apple");
+        bot.put("rating", "1500");
+        return bot;
+    }
+}
