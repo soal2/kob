8472dca80f334f45aab423d40fe5c1d37fb02647
Formatting <2339497599@qq.com>
2024-04-29 14:53:09 +0800

    创建后端

--- backend/src/main/java/com/kob/backend/Controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/Controller/pk/IndexController.java
@@ -1,15 +1,13 @@
-package com.kob.backend.Controller.pk;
+package com.kob.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pk/")  //指定父目录
-public class IndexController {
-
-    @RequestMapping("index/")
-    public String index()
-    {
+@RequestMapping("/pk/")
+public class indexController {
+@RequestMapping("index/")
+    public String index(){
         return "pk/index.html";
     }
 }
