700d30b8429fc124ff4cbda665c48aa59f12cddc
Formatting <2339497599@qq.com>
2024-05-07 16:02:59 +0800

    配置Mysql与注册登录模块(上)

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,6 +1,5 @@
-package com.kob.backend.Controller.pk;
+package com.kob.backend.controller.pk;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/pk/")
-public class indexController {
+public class IndexController {
 @RequestMapping("index/")
     public String index(){
         return "pk/index.html";
